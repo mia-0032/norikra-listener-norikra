6d1d0c7bed0476b44316983dcbf01b43f71fa7b6
Yoshihiro MIYAI <msparrow17@gmail.com>
2015-10-21 02:24:02 +0900

    Add parse_argument test

--- lib/norikra/listener/norikra.rb
+++ lib/norikra/listener/norikra.rb
@@ -8,16 +8,21 @@ module Norikra
         "NORIKRA"
       end
 
-      def initialize(argument, query_name, query_group)
-        super
+      def self.parse_argument(argument, query_name)
         addr, target = argument.split(',', 2)
         target = query_name unless target
         @target = target
 
         host, port_str = addr.split(':', 2)
-        raise Norikra::ArgumentError, "host is not specified" unless host
+        raise Norikra::ArgumentError, "host is not specified" if host.empty?
         port = port_str ? port_str.to_i : 26571
 
+        [target, host, port]
+      end
+
+      def initialize(argument, query_name, query_group)
+        super
+        @target, host, port = NorikraForwarder::parse_argument(argument, query_name)
         @client = Norikra::Client.new(host, port)
       end
 
--- spec/norikra_spec.rb
+++ spec/norikra_spec.rb
@@ -6,7 +6,33 @@ require 'norikra/listener_spec_helper'
 describe Norikra::Listener::NorikraForwarder do
   describe '.label' do
     it 'returns "NORIKRA"' do
-      expect(Norikra::Listener::NorikraForwarder.label).to eql("NORIKRA")
+      expect(Norikra::Listener::NorikraForwarder.label).to eql('NORIKRA')
+    end
+  end
+
+  describe '.parse_argument' do
+    it 'only host specified' do
+      expect do
+          Norikra::Listener::NorikraForwarder.parse_argument(':1', 'test_query')
+      end.to raise_error(Norikra::ArgumentError, 'host is not specified')
+    end
+
+    it 'only host specified' do
+      expect(
+          Norikra::Listener::NorikraForwarder.parse_argument('localhost', 'test_query')
+      ).to eql(['test_query', 'localhost', 26571])
+    end
+
+    it 'target is not specified' do
+      expect(
+          Norikra::Listener::NorikraForwarder.parse_argument('localhost:22222', 'test_query')
+      ).to eql(['test_query', 'localhost', 22222])
+    end
+
+    it 'all are specified' do
+      expect(
+          Norikra::Listener::NorikraForwarder.parse_argument('localhost:22222,test_target', 'test_query')
+      ).to eql(['test_target', 'localhost', 22222])
     end
   end
 end
